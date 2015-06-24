6c10e57a4aa26dd845520d860075f669c09fc5f1
Tyler Parker <tparker42d2@gmail.com>
2015-06-24 11:50:56 -0600

    migrated models

--- db/migrate/20150624174121_create_items.rb
+++ db/migrate/20150624174121_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-
+      t.decimal :price, precision: 6, scale: 2, null: false
+      t.text :contact, null: false
+      t.string :city
+      t.string :quality
+      t.string :name_of_item, null: false
+      t.text :description
       t.timestamps null: false
     end
   end
